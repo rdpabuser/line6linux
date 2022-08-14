fc3be32127ac900e8c63ed11c7ee7eb7d575a1e3
rdpabuser <dykogithub@proton.me>
2022-08-14 16:02:37 +0200

    Initial commit

---
+++ src/line6unix.c
@@ -0,0 +1,9 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/usb.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("rdpabuser");
+MODULE_DESCRIPTION("Line6 products support for Linux.");
+MODULE_VERSION("0.01");
